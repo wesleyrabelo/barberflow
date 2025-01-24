b3024a4afa596c570e2445180d17a120c8259edd
wesley <wesleyrab2020@gmail.com>
2025-01-24 11:34:16 -0300

    Adicao das anotacoes @Id e @GeneratedValue

--- barberflow/src/main/java/com/barberflow/barberflow/customer/entity/Customer.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/entity/Customer.java
@@ -2,6 +2,9 @@ package com.barberflow.barberflow.customer.entity;
 
 import com.barberflow.barberflow.appointment.Appointment;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +18,8 @@ import java.util.List;
 @Getter
 @Setter
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;

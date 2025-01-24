7dc17cd1735e2cc4003ae0dad03864b629630413
wesley <wesleyrab2020@gmail.com>
2025-01-24 12:37:56 -0300

    Declaracao manual dos getters, setters e construtores

--- barberflow/src/main/java/com/barberflow/barberflow/customer/entity/Customer.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/entity/Customer.java
@@ -13,10 +13,6 @@ import lombok.Setter;
 import java.util.List;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,5 +21,54 @@ public class Customer {
     private String email;
     private String password;
     private String phoneNumber;
-    private List<Appointment> appointments;
+    //private List<Appointment> appointments;
+
+    public Customer(){}
+
+    public Customer(String name, String email, String password, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }

cf410d9cbee239d946ad19b1f739618ffa2de52e
wesley <wesleyrab2020@gmail.com>
2025-01-31 14:05:45 -0300

    Mudança no metodo signupCustomer
    Alteracao para receber o signupReceiveDto ao inves do objeto Customer.
    Insercao do campo CustomerMapper.

--- barberflow/src/main/java/com/barberflow/barberflow/customer/controller/CustomerController.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/controller/CustomerController.java
@@ -1,23 +1,30 @@
 package com.barberflow.barberflow.customer.controller;
 
+import com.barberflow.barberflow.customer.dto.customer.CustomerSignupReceiveDto;
+import com.barberflow.barberflow.customer.dto.mapper.CustomerMapper;
 import com.barberflow.barberflow.customer.entity.Customer;
 import com.barberflow.barberflow.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//TO-DO : Criar DTOs, adicionar RestControllerAdvice
+
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
     private final CustomerService customerService;
+    private final CustomerMapper customerMapper;
 
     @Autowired
-    public CustomerController(CustomerService customerService){
+    public CustomerController(CustomerService customerService, CustomerMapper customerMapper){
         this.customerService = customerService;
+        this.customerMapper = customerMapper;
     }
 
     @PostMapping
-    public ResponseEntity<Customer> signupCustomer(@RequestBody Customer customer){
+    public ResponseEntity<Customer> signupCustomer(@RequestBody CustomerSignupReceiveDto dto){
+        Customer customer = customerMapper.signupDtoToCustomer(dto);
         return ResponseEntity.ok().body(customerService.saveCustomer(customer));
     }
 

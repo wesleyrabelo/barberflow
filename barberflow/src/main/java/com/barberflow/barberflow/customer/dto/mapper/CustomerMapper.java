b5a8ad2f2ac5f34e6fcfaa3c2f1a346a7b8694f8
wesley <wesleyrab2020@gmail.com>
2025-02-03 13:46:17 -0300

    Criacao do metodo que converte Customer para FindByIdResponseDto

--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/mapper/CustomerMapper.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/mapper/CustomerMapper.java
@@ -1,5 +1,6 @@
 package com.barberflow.barberflow.customer.dto.mapper;
 
+import com.barberflow.barberflow.customer.dto.find.CustomerFindByIdResponseDto;
 import com.barberflow.barberflow.customer.dto.signup.CustomerSignupReceiveDto;
 import com.barberflow.barberflow.customer.dto.signup.CustomerSignupResponseDto;
 import com.barberflow.barberflow.customer.dto.update.CustomerUpdateReceiveDto;
@@ -16,4 +17,5 @@ public interface CustomerMapper {
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Customer updateCustomerFromDto(CustomerUpdateReceiveDto dto, @MappingTarget Customer customer);
     CustomerUpdateResponseDto customerToUpdateResponseDto(Customer customer);
+    CustomerFindByIdResponseDto customerToCustomerFindByIdResponseDto(Customer customer);
 }

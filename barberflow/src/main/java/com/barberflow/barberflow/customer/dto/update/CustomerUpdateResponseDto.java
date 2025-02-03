8fd45c5f0c75a9e99219723153b24284baafcb73
wesley <wesleyrab2020@gmail.com>
2025-02-03 10:29:07 -0300

    Criacao da classe responsavel por responder ao update dos Customers

---
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/update/CustomerUpdateResponseDto.java
@@ -0,0 +1,9 @@
+package com.barberflow.barberflow.customer.dto.update;
+
+public class CustomerUpdateResponseDto {
+    public Long id;
+    public String name;
+    public String email;
+    public String password;
+    public String phoneNumber;
+}

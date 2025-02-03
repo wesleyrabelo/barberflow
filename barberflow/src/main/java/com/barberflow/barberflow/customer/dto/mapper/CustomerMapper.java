1f2a8e6eb8368ede241339b2fb9649899433ba2b
wesley <wesleyrab2020@gmail.com>
2025-02-03 10:56:01 -0300

    Criacao do metodo updateCustomerFromDto

--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/mapper/CustomerMapper.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/mapper/CustomerMapper.java
@@ -2,8 +2,9 @@ package com.barberflow.barberflow.customer.dto.mapper;
 
 import com.barberflow.barberflow.customer.dto.signup.CustomerSignupReceiveDto;
 import com.barberflow.barberflow.customer.dto.signup.CustomerSignupResponseDto;
+import com.barberflow.barberflow.customer.dto.update.CustomerUpdateReceiveDto;
 import com.barberflow.barberflow.customer.entity.Customer;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
@@ -11,4 +12,6 @@ import org.springframework.stereotype.Component;
 public interface CustomerMapper {
     Customer customerReceiveDtoToCustomer(CustomerSignupReceiveDto dto);
     CustomerSignupResponseDto customerToSignupResponseDto(Customer customer);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Customer updateCustomerFromDto(CustomerUpdateReceiveDto dto, @MappingTarget Customer customer);
 }

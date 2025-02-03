7f3344c6bdd6aab61a5e72b52fcbd37d8d13292f
wesley <wesleyrab2020@gmail.com>
2025-02-03 14:11:29 -0300

    Criado metodo para mapear objetos Customer para CustomerFindByNameResponseDto

--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/mapper/CustomerMapper.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/mapper/CustomerMapper.java
@@ -1,6 +1,7 @@
 package com.barberflow.barberflow.customer.dto.mapper;
 
 import com.barberflow.barberflow.customer.dto.find.CustomerFindByIdResponseDto;
+import com.barberflow.barberflow.customer.dto.find.CustomerFindByNameResponseDto;
 import com.barberflow.barberflow.customer.dto.signup.CustomerSignupReceiveDto;
 import com.barberflow.barberflow.customer.dto.signup.CustomerSignupResponseDto;
 import com.barberflow.barberflow.customer.dto.update.CustomerUpdateReceiveDto;
@@ -18,4 +19,5 @@ public interface CustomerMapper {
     Customer updateCustomerFromDto(CustomerUpdateReceiveDto dto, @MappingTarget Customer customer);
     CustomerUpdateResponseDto customerToUpdateResponseDto(Customer customer);
     CustomerFindByIdResponseDto customerToCustomerFindByIdResponseDto(Customer customer);
+    CustomerFindByNameResponseDto customerToCustomerFindByNameResponseDto(Customer customer);
 }

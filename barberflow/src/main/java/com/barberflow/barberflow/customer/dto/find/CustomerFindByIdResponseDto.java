f44f8b5f2a6410554a925c8d89bc300a8ee5ab8c
wesley <wesleyrab2020@gmail.com>
2025-02-03 13:45:25 -0300

    Criacao da classe CustomerFindByIdResponseDto

---
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/find/CustomerFindByIdResponseDto.java
@@ -0,0 +1,9 @@
+package com.barberflow.barberflow.customer.dto.find;
+
+//TO-DO: Adicionar os Appointments quando eles forem criados
+public class CustomerFindByIdResponseDto {
+    public String name;
+    public String email;
+    public String password;
+    public String phoneNumber;
+}

7b138e43f4424cccd87dfab487ce8d2134528799
wesley <wesleyrab2020@gmail.com>
2025-02-03 11:06:37 -0300

    Retirada o campo ID

--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/update/CustomerUpdateResponseDto.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/update/CustomerUpdateResponseDto.java
@@ -2,7 +2,6 @@ package com.barberflow.barberflow.customer.dto.update;
 
 //TO-DO: Adicionar restrições?
 public class CustomerUpdateResponseDto {
-    public Long id;
     public String name;
     public String email;
     public String password;

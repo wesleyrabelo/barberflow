d4432b1c0317d2e2ef9bf48fa5e26b08af599271
wesley <wesleyrab2020@gmail.com>
2025-02-03 13:56:32 -0300

    Criado dto que transportara a resposta do metodo findByEmail

---
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/find/CustomerFindByEmailResponseDto.java
@@ -0,0 +1,9 @@
+package com.barberflow.barberflow.customer.dto.find;
+
+//Adicionar os appointments quando criados
+public class CustomerFindByEmailResponseDto {
+    public String name;
+    public String email;
+    public String password;
+    public String phoneNumber;
+}

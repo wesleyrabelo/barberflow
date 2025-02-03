f378283d2be4de26876ca0ca6574dda2ea8a280c
wesley <wesleyrab2020@gmail.com>
2025-02-03 10:28:45 -0300

    Alteracao dos campos de private para public

--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/update/CustomerUpdateReceiveDto.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/update/CustomerUpdateReceiveDto.java
@@ -2,8 +2,8 @@ package com.barberflow.barberflow.customer.dto.update;
 
 //TO-DO: Adicionar restrições?
 public class CustomerUpdateReceiveDto {
-    private String name;
-    private String email;
-    private String password;
-    private String phoneNumber;
+    public String name;
+    public String email;
+    public String password;
+    public String phoneNumber;
 }

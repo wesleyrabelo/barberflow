a4ab2e3b4653b0cb6ccdf0e4bcfa4959e87b697d
wesley <wesleyrab2020@gmail.com>
2025-02-04 13:22:08 -0300

    Adicionado metodo para tratar a EntityNotFoundException
    CustomerExceptionController possui o metodo para tratar a
    EntityNotFoundException. Alem de ser adicionado o pacote em que a classe
    fará o tratamento das exceções.

--- barberflow/src/main/java/com/barberflow/barberflow/customer/controller/CustomerExceptionController.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/controller/CustomerExceptionController.java
@@ -1,4 +1,15 @@
 package com.barberflow.barberflow.customer.controller;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("com.barberflow.barberflow.customer")
 public class CustomerExceptionController {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> entityNotFoundException(EntityNotFoundException exception){
+        return ResponseEntity.status(HttpStatusCode.valueOf(500)).body(exception.getMessage());
+    }
 }

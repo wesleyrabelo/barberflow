9a04afd3038ed100249981cb3301e5e8db01b7ab
wesley <wesleyrab2020@gmail.com>
2025-02-05 11:02:05 -0300

    Adicao de restricoes no name do CustomerSignupReceiveDto

--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/signup/CustomerSignupReceiveDto.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/signup/CustomerSignupReceiveDto.java
@@ -1,10 +1,14 @@
 package com.barberflow.barberflow.customer.dto.signup;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 //TO-DO: Adicionar as restrições na criação do campo
 public class CustomerSignupReceiveDto {
-    @NotBlank
+    @NotBlank(message = "Nome não pode ser vazio ou nulo")
+    @Size(min = 6, max = 20, message = "Tamanho do nome deve ser entre 6 e 20 caracteres")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Nome deve conter somente letras")
     public String name;
     public String email;
     public String password;

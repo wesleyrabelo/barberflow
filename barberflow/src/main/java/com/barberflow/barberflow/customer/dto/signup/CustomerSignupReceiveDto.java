289133323ccd05cd39b2d45cda24d97bb657ecc1
wesley <wesleyrab2020@gmail.com>
2025-02-05 13:54:39 -0300

    Validacoes adicionadas na classe CustomerUpdateReceiveDto

--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/signup/CustomerSignupReceiveDto.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/signup/CustomerSignupReceiveDto.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-//TO-DO: Adicionar as restrições na criação do campo
 public class CustomerSignupReceiveDto {
     @NotBlank(message = "Nome não pode ser vazio ou nulo")
     @Size(min = 6, max = 20, message = "Tamanho do nome deve ser entre 6 e 20 caracteres")
--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/update/CustomerUpdateReceiveDto.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/update/CustomerUpdateReceiveDto.java
@@ -1,9 +1,24 @@
 package com.barberflow.barberflow.customer.dto.update;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 //TO-DO: Adicionar restrições? - Adicionar os Appointments quando eles forem criados
 public class CustomerUpdateReceiveDto {
+    @Nullable
+    @Size(min = 6, max = 20, message = "Tamanho do nome deve ser entre 6 e 20 caracteres")
+    @Pattern(regexp = "^[a-zA-Z ,.'-]+$", message = "Nome deve conter somente letras")
     public String name;
+    @Nullable
+    @Pattern(regexp = "^[a-z0-9.]+@[a-z0-9]+\\.[a-z]+(\\.[a-z]+)?$", message = "Deve ser um email válido")
     public String email;
+    @Nullable
+    @Size(max = 15, message = "Tamanho máximo é 15 caracteres")
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Senha inválida")
     public String password;
+    @Nullable
+    @Pattern(regexp = "^[1-9][1-9][0-9]{9}$", message = "Número de telefone inválido")
     public String phoneNumber;
 }

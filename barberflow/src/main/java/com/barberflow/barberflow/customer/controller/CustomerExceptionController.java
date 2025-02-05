1ffff5f2c3c79b8241fa21add8fb46046a8fbc84
wesley <wesleyrab2020@gmail.com>
2025-02-05 11:17:44 -0300

    Tratamento da excecao MethodArgumentNotValidException no CustomerExceptionController

--- barberflow/src/main/java/com/barberflow/barberflow/customer/controller/CustomerExceptionController.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/controller/CustomerExceptionController.java
@@ -3,23 +3,40 @@ package com.barberflow.barberflow.customer.controller;
 import com.barberflow.barberflow.customer.exception.CustomerExceptionResponse;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @RestControllerAdvice("com.barberflow.barberflow.customer")
 public class CustomerExceptionController {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<CustomerExceptionResponse> entityNotFoundException(EntityNotFoundException exception){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                new CustomerExceptionResponse(
-                        exception.getClass().getSimpleName(),
-                        exception.getMessage(),
-                        LocalDateTime.now(),
-                        HttpStatus.NOT_FOUND)
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(
+                    new CustomerExceptionResponse(
+                            exception.getClass().getSimpleName(),
+                            exception.getMessage(),
+                            LocalDateTime.now(),
+                            HttpStatus.NOT_FOUND)
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomerExceptionResponse> methodArgumentNotValidException(MethodArgumentNotValidException exception){
+        return ResponseEntity
+                .badRequest()
+                .body(
+                    new CustomerExceptionResponse(
+                            exception.getClass().getSimpleName(),
+                            Objects.requireNonNull(exception.getFieldError()).getDefaultMessage(),
+                            LocalDateTime.now(),
+                            HttpStatus.BAD_REQUEST
+                    )
         );
     }
 }

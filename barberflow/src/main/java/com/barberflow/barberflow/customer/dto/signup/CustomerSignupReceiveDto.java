124b0e07892ba1dbbc6fa18cad3f167de67b6a27
wesley <wesleyrab2020@gmail.com>
2025-02-05 11:17:09 -0300

    Alteracao no Regex do campo name e aplicacao do metodo trim() ao criar a classe

--- barberflow/src/main/java/com/barberflow/barberflow/customer/dto/signup/CustomerSignupReceiveDto.java
+++ barberflow/src/main/java/com/barberflow/barberflow/customer/dto/signup/CustomerSignupReceiveDto.java
@@ -8,16 +8,16 @@ import jakarta.validation.constraints.Size;
 public class CustomerSignupReceiveDto {
     @NotBlank(message = "Nome não pode ser vazio ou nulo")
     @Size(min = 6, max = 20, message = "Tamanho do nome deve ser entre 6 e 20 caracteres")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Nome deve conter somente letras")
+    @Pattern(regexp = "^[a-z ,.'-]+$", message = "Nome deve conter somente letras")
     public String name;
     public String email;
     public String password;
     public String phoneNumber;
 
     public CustomerSignupReceiveDto(String name, String email, String password, String phoneNumber) {
-        this.name = name;
-        this.email = email;
+        this.name = name.trim();
+        this.email = email.trim();
         this.password = password;
-        this.phoneNumber = phoneNumber;
+        this.phoneNumber = phoneNumber.trim();
     }
 }

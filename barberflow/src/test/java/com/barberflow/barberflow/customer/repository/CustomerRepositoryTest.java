d17bfb2751612eb5058c3375f3bb8c53ec9e577e
wesley <wesleyrab2020@gmail.com>
2025-02-27 16:08:04 -0300

    Teste do metodo findByName

---
+++ barberflow/src/test/java/com/barberflow/barberflow/customer/repository/CustomerRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.barberflow.barberflow.customer.repository;
+
+import com.barberflow.barberflow.customer.entity.Customer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class CustomerRepositoryTest {
+    @Autowired
+    private CustomerRepository repository;
+
+    @BeforeEach
+    void setUp(){
+        Customer customer = new Customer(
+                "john doe",
+                "john@email.com",
+                "12345678aA!",
+                "12121212121"
+        );
+        repository.save(customer);
+    }
+
+    @Test
+    void findByName_returnSavedCustomer(){
+        Customer saved = repository.findByName("john doe").orElse(null);
+
+        Assertions.assertThat(saved.getId()).isEqualTo(1L);
+        Assertions.assertThat(saved.getName()).isEqualTo("john doe");
+        Assertions.assertThat(saved.getPassword()).isEqualTo("12345678aA!");
+        Assertions.assertThat(saved.getPhoneNumber()).isEqualTo("12121212121");
+    }
+}

a959c6c702800b4ef897fea9640c966570004599
wesley <wesleyrab2020@gmail.com>
2025-03-05 21:04:49 -0300

    Criacao da clase CustomerServiceTest

--- barberflow/src/test/java/com/barberflow/barberflow/customer/repository/CustomerRepositoryTest.java
+++ barberflow/src/test/java/com/barberflow/barberflow/customer/repository/CustomerRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
